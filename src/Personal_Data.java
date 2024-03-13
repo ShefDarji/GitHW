0a769aefd01b4854397aba2d05b0f0f7ddb9966b
Shefali Darji <shefalidarji21@gmail.com>
2024-03-13 21:33:46 +0000

    First Commit

---
+++ src/Personal_Data.java
@@ -0,0 +1,26 @@
+
+public class Personal_Data {
+
+    public static void main (String [] args){
+        String name = "Sherlock Holmes";
+        char gender = 'M';
+        int age = 35;
+        String address = "221 Baker Street";
+        String mobile_number = "+44 2388779966";
+        String job = "Special Agent";
+        double salary = 45000.45;
+        char currency = '£';
+
+
+        System.out.println("My name is " + name);
+        System.out.println("My gender is " + gender);
+        System.out.println("Age is " + age);
+        System.out.println("I live at " + address);
+        System.out.println("My contact number is "+ mobile_number);
+        System.out.println("I work as a " + job);
+        System.out.println("My yearly package is " + salary + " in " + currency);
+
+    }
+}
+
+
